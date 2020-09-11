fc2b3c910713747dca48409331087dedde89be19
Mitu <mitu@glowup.games>
2020-09-10 23:57:40 -0400

    Added all current project files - basic sprite animation

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This means we can attach it to a GameObject
+public class ColorChanger : MonoBehaviour 
+{
+
+    public Color dogColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().color = dogColor;
+    }
+}
---
+++ Assets/Scripts/DogMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+           transform.Translate(Vector3.right * speed);
+           GetComponent<Animator>().Play("walk_right");
+
+
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed);
+            GetComponent<Animator>().Play("walk_left");
+
+        } else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * speed);
+            GetComponent<Animator>().Play("walk_down");
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * speed);
+            GetComponent<Animator>().Play("walk_up");
+        } else
+        {
+            GetComponent<Animator>().Play("idle");
+        }
+
+
+
+    }
+}
