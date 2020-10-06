5d8f720676235516f12d4a98218316fc8124b0ba
Mitu <mitu@glowup.games>
2020-10-06 12:20:27 -0400

    Added a GameManager w/ a singleton structure.

--- NYCProject/Assets/Scripts/AnimalMovement.cs
+++ NYCProject/Assets/Scripts/AnimalMovement.cs
@@ -16,74 +16,110 @@ public class AnimalMovement : MonoBehaviour
     public string NoiseToMake;
 
     public Rigidbody2D rigidbody;
+
     
 
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+
+        if (GameManager.gmSingleton != null)
+        {
+            Debug.Log("GameManager is ready to use.");
+            rigidbody = GetComponent<Rigidbody2D>();
+
+        }
     }
 
 
     private void FixedUpdate()
     {
-        
-        if(Input.GetKey(right))
-        {
 
-            rigidbody.AddForce(Vector2.right);
+            if (Input.GetKey(right))
+            {
 
-        } else if (Input.GetKey(left))
-        {
-            rigidbody.AddForce(Vector2.left);
-        
-          } else if (Input.GetKey(up))
-        {
-            rigidbody.AddForce(Vector2.up);
-        
-        } else if (Input.GetKey(down))
-        {
-            rigidbody.AddForce(Vector2.down);
-        } 
+                //rigidbody.AddForce(Vector2.right * speed);
+                rigidbody.velocity = new Vector2(1 * speed, rigidbody.velocity.y > -4.0f ? rigidbody.velocity.y : -4.0f);
+                               Debug.Log("Move right");
+
+            }
+            else if (Input.GetKey(left))
+            {
+                rigidbody.velocity = new Vector2(-1 * speed, rigidbody.velocity.y > -4.0f ? rigidbody.velocity.y : -4.0f);
+
+                Debug.Log("Move left");
+            }
+            else if (Input.GetKey(up))
+            {
+                rigidbody.AddForce(Vector2.up * speed);
+
+            }
+            else if (Input.GetKey(down))
+            {
+                rigidbody.AddForce(Vector2.down * speed);
+            }
+
+            else
+            {
+                rigidbody.velocity = Vector2.zero;
+            }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(right))
-        {
-           //transform.Translate(Vector3.right * speed);
-           GetComponent<Animator>().Play("walk_right");
 
 
-        } else if (Input.GetKey(left))
+        if (!GameManager.gmSingleton.hasGameStarted)
         {
-           // transform.Translate(Vector3.left * speed);
-            GetComponent<Animator>().Play("walk_left");
-
-        } else if (Input.GetKey(down))
-        {
-           // transform.Translate(Vector3.down * speed);
-            GetComponent<Animator>().Play("walk_down");
-        }
-        else if (Input.GetKey(up))
-        {
-           // transform.Translate(Vector3.up * speed);
-            GetComponent<Animator>().Play("walk_up");
-        } else
-        {
-            GetComponent<Animator>().Play("idle");
+            if (Input.GetKey(KeyCode.Space))
+            {
+                GameManager.gmSingleton.hasGameStarted = true;
+            }
         }
 
-        if(Input.GetKeyDown(up) || Input.GetKeyDown(down) || Input.GetKeyDown(left) || Input.GetKeyDown(right))
+        if (GameManager.gmSingleton.hasGameStarted)
         {
-            textLabel.text = "";
-        }
 
-        //set the position of the textLabel to the same as the position of THIS animal
-        textLabel.transform.position = transform.position;
+            if (Input.GetKey(right))
+            {
+                //transform.Translate(Vector3.right * speed);
+                GetComponent<Animator>().Play("walk_right");
+
+
+            }
+            else if (Input.GetKey(left))
+            {
+                // transform.Translate(Vector3.left * speed);
+                GetComponent<Animator>().Play("walk_left");
+
+            }
+            else if (Input.GetKey(down))
+            {
+                // transform.Translate(Vector3.down * speed);
+                GetComponent<Animator>().Play("walk_down");
+            }
+            else if (Input.GetKey(up))
+            {
+                // transform.Translate(Vector3.up * speed);
+                GetComponent<Animator>().Play("walk_up");
+            }
+            else
+            {
+                GetComponent<Animator>().Play("idle");
+            }
+
+            if (Input.GetKeyDown(up) || Input.GetKeyDown(down) || Input.GetKeyDown(left) || Input.GetKeyDown(right))
+            {
+                textLabel.text = "";
+            }
+
+            //set the position of the textLabel to the same as the position of THIS animal
+            textLabel.transform.position = transform.position;
+        }
 
     }
 
@@ -95,7 +131,14 @@ public class AnimalMovement : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+    
+        Debug.Log(gameObject.name + " has collided with " + collision.gameObject.name);
         PressButtonToMakeNoise();
+        score++;
+
+        GameManager.gmSingleton.noOfCollisions++;
+
+
     }
 
 }
---
+++ NYCProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int score = 0;
+    public bool hasGameStarted = false;
+    public int noOfCollisions = 0;
+
+
+    public static GameManager gmSingleton;
+
+
+    void Awake()
+    {
+        gmSingleton = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("No. of collisions " + noOfCollisions);
+    }
+}
